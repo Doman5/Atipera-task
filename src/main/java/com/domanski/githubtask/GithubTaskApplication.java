ffef8b4587724927225d5061b970c97215ba1211
Dominik <dono3108@gmail.com>
2023-06-06 12:24:52 +0200

    init commit

---
+++ src/main/java/com/domanski/githubtask/GithubTaskApplication.java
@@ -0,0 +1,11 @@
+package com.domanski.githubtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubTaskApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GithubTaskApplication.class);
+    }
+}
