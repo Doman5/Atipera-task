96981f77b9624c50b6853ea5f123b0b36df8bb8e
Dominik <dono3108@gmail.com>
2023-06-06 12:33:29 +0200

    create github controller with header checker

---
+++ src/main/java/com/domanski/githubtask/exception/UnsupportedMediaTypeException.java
@@ -0,0 +1,7 @@
+package com.domanski.githubtask.exception;
+
+public class UnsupportedMediaTypeException extends RuntimeException {
+    public UnsupportedMediaTypeException(String message) {
+        super(message);
+    }
+}
