e4757764e9e885e206b63106f00f8959c668fa2c
Dominik <dono3108@gmail.com>
2023-06-09 14:34:24 +0200

    change exception throwed in controller

--- src/main/java/com/domanski/githubtask/controller/GithubController.java
+++ src/main/java/com/domanski/githubtask/controller/GithubController.java
@@ -1,14 +1,17 @@
 package com.domanski.githubtask.controller;
 
-import com.domanski.githubtask.exception.UnsupportedMediaTypeException;
+
 import com.domanski.githubtask.controller.dto.UserRepoResponse;
 import com.domanski.githubtask.service.GithubService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
@@ -17,22 +20,12 @@ public class GithubController {
 
     private final GithubService githubService;
 
-    @GetMapping(value = "/github/{username}")
-    public ResponseEntity<List<UserRepoResponse>> getUserRepositoryWithRequestParam(@PathVariable String username,
-                                                                    @RequestHeader(HttpHeaders.ACCEPT) MediaType acceptHeader) {
-        if (acceptHeader.equals(MediaType.APPLICATION_XML)) {
-            throw new UnsupportedMediaTypeException("%s is unsupported media type".formatted(acceptHeader));
-        }
-
-        List<UserRepoResponse> finalResponses = githubService.getUserRepositoriesWithBranches(username);
-        return ResponseEntity.ok(finalResponses);
-    }
 
     @GetMapping(value = "/github")
     public ResponseEntity<List<UserRepoResponse>> getUserRepositoryWithRequestBody(@RequestBody String username,
-                                                                    @RequestHeader(HttpHeaders.ACCEPT) MediaType acceptHeader) {
+                                                                    @RequestHeader(HttpHeaders.ACCEPT) MediaType acceptHeader) throws HttpMediaTypeNotAcceptableException {
         if (acceptHeader.equals(MediaType.APPLICATION_XML)) {
-            throw new UnsupportedMediaTypeException("%s is unsupported media type".formatted(acceptHeader));
+            throw new HttpMediaTypeNotAcceptableException("%s is unsupported media type".formatted(acceptHeader));
         }
 
         List<UserRepoResponse> finalResponses = githubService.getUserRepositoriesWithBranches(username);
--- src/main/java/com/domanski/githubtask/controller/error/GithubControllerErrorHandler.java
+++ src/main/java/com/domanski/githubtask/controller/error/GithubControllerErrorHandler.java
@@ -1,11 +1,12 @@
-package com.domanski.githubtask.controller;
+package com.domanski.githubtask.controller.error;
 
-import com.domanski.githubtask.exception.UnsupportedMediaTypeException;
+import com.domanski.githubtask.controller.GithubController;
 import com.domanski.githubtask.exception.UserDoNotHaveRepositoriesException;
 import com.domanski.githubtask.exception.UsernameNoExistException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,19 +15,21 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ControllerAdvice(basePackageClasses = GithubController.class)
 public class GithubControllerErrorHandler {
 
-    @ExceptionHandler(UnsupportedMediaTypeException.class)
+    @ExceptionHandler(HttpMediaTypeNotAcceptableException.class)
     @ResponseBody
-    public ResponseEntity<GithubErrorResponse> handleUnsupportedMediaTypeException(UnsupportedMediaTypeException ex) {
+    public ResponseEntity<GithubErrorResponse> handleUnsupportedMediaTypeException(HttpMediaTypeNotAcceptableException ex) {
         GithubErrorResponse error = new GithubErrorResponse(HttpStatus.NOT_ACCEPTABLE, ex.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).contentType(MediaType.APPLICATION_JSON).body(error);
     }
 
     @ExceptionHandler(UserDoNotHaveRepositoriesException.class)
     @ResponseBody
-    @ResponseStatus(HttpStatus.CONFLICT)
+
     public ResponseEntity<GithubErrorResponse> handleUserDoNotHaveRepoException(UserDoNotHaveRepositoriesException e) {
-        GithubErrorResponse responseError = new GithubErrorResponse(HttpStatus.NOT_ACCEPTABLE, e.getMessage());
-        return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).contentType(MediaType.APPLICATION_JSON).body(responseError);
+        GithubErrorResponse responseError = new GithubErrorResponse(HttpStatus.CONFLICT, e.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(responseError);
     }
 
     @ExceptionHandler(UsernameNoExistException.class)
