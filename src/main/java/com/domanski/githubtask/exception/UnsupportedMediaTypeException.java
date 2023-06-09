4b356b6bc3eab9baf5b88809d578d9585aa4d86a
Dominik <dono3108@gmail.com>
2023-06-09 14:35:36 +0200

    delete redundant exception class

--- src/main/java/com/domanski/githubtask/exception/UnsupportedMediaTypeException.java
+++
@@ -1,7 +0,0 @@
-package com.domanski.githubtask.exception;
-
-public class UnsupportedMediaTypeException extends RuntimeException {
-    public UnsupportedMediaTypeException(String message) {
-        super(message);
-    }
-}
